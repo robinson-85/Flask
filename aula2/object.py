d84da1c23b599cb6027bc3f4cbc18eaa9df10032
robinson-85 <robinpaulista@yahoo.com.br>
2020-07-02 21:07:16 -0300

    object.py

---
+++ aula2/object.py
@@ -0,0 +1,13 @@
+class Animal:
+    # atributos de classe
+    planeta = "Terra"
+
+    # metodo
+    def nascer(self):
+        print(f"Oi, eu nasci na{self.planeta}")
+
+    def comer(self):
+        print("Estou comendo...crunch crunch")
+
+https://www.twitch.tv/videos/645042904?collection=gRe7fj7iGBZJMQ
+
