ee70ba9ee9ff22c2b860e3556e9960519b38caf8
robinson-85 <robinpaulista@yahoo.com.br>
2020-07-03 00:25:20 -0300

    object.py

--- aula2/object.py
+++ aula2/object.py
@@ -4,10 +4,12 @@ class Animal:
 
     # metodo
     def nascer(self):
-        print(f"Oi, eu nasci na{self.planeta}")
+        print(f"Oi, eu nasci na {self.planeta}")
 
     def comer(self):
         print("Estou comendo...crunch crunch")
 
 https://www.twitch.tv/videos/645042904?collection=gRe7fj7iGBZJMQ
+
+
 
