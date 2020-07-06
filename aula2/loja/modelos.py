fd4ae41b5f19dffb6c0c8a8ce7b614765cc04c7e
robinson-85 <robinpaulista@yahoo.com.br>
2020-07-06 16:57:41 -0300

    __initi__.py

---
+++ aula2/loja/modelos.py
@@ -0,0 +1,9 @@
+class Categoria:
+    def__init__(self, nome):
+        self.nome = nome 
+
+
+class Produto:
+    def__init__(self, nome,categoria):
+        self.nome = nome 
+        self.categoria = categoria
--- aula2/produtos.py
+++ aula2/produtos.py
@@ -1,12 +1,6 @@
-class Categoria:
-    def__init__(self, nome):
-        self.nome = nome 
 
-
-class Produto:
-    def__init__(self, nome,categoria):
-        self.nome = nome 
-        self.categoria = categoria
+import loja
 
 cadeira = Produto("Cadeira", categoria=Categoria("Moveis"))
 teclado = Produto("HiperX", categoria=Categoria("Eletronico"))
+
