be09a1feceaa39a558229cf72b4d54bc3b987845
robinson-85 <robinpaulista@yahoo.com.br>
2020-08-25 12:09:06 -0300

    Aula 4 - parte 1

---
+++ aula4_pt1/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+import views
+
+
+def create_app():
+    """Factory principal"""
+
+    app = Flask(__name__)
+    views.init_app(app)
+
+    return app
---
+++ aula4_pt1/views.py
@@ -0,0 +1,16 @@
+"""Extensão Flask"""
+from flask import Flask, request
+
+
+def init_app(app: Flask):
+    
+    """Inicialização de extensões"""
+
+    @app.route("/")
+    def index():
+        print(request.args)
+        return "Hello Codeshow"
+
+    @app.route("/contato")
+    def contato():
+        return "<form><input type='text'></input></form>"
